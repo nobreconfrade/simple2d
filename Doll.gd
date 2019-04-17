71236716952588dc24df74fde33f3ec88b614a72
nobreconfrade <willpereirabr@gmail.com>
2019-04-16 22:21:05 -0300

    Smooth moviment and character sprites. simple2d_p3

--- Doll.gd
+++ Doll.gd
@@ -1,27 +1,44 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1)
+const GRAVITY = 20
+const ACCEL = 50
+const MAX_SPEED = 200
+const JUMP_HEIGHT = -550
 var motion = Vector2() 
 
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += GRAVITY
+	var friction = false
+	
 	if(Input.is_action_pressed("ui_right")):
-		motion.x = 100
+		motion.x += ACCEL
+		motion.x = min(motion.x, MAX_SPEED)
 		$doll_sprite.flip_h = false
 		$doll_sprite.play('Run')
 	elif(Input.is_action_pressed("ui_left")):
-		motion.x = -100
+		motion.x -= ACCEL
+		motion.x = -MAX_SPEED if (motion.x < -MAX_SPEED) else motion.x
 		$doll_sprite.flip_h = true
 		$doll_sprite.play('Run')		
 	else:
 		$doll_sprite.play('Idle')
-		motion.x = 0
+		friction = true
+		
 	
 	if(is_on_floor()):
 		motion.y = 0.5
 		if(Input.is_action_just_pressed("ui_up")):
-			motion.y = -300
+			motion.y = JUMP_HEIGHT
+		if(friction == true):
+			motion.x = lerp(motion.x, 0, 0.2)
+			
 	else:
-		$doll_sprite.play('Jump')
-	
+		if (motion.y < 0):
+			$doll_sprite.play('Jump')
+		else:
+			$doll_sprite.play('Fall')			
+		if(friction == true):
+			motion.x = lerp(motion.x, 0, 0.05)
+				
 	motion = move_and_slide(motion, UP)
